1bf32bf681c1896bf92b86a2ef10c4ed81604aa9
testuser250918-code <testuser250918@gmail.com>
2026-03-17 19:25:50 +0900

    [feat] 파일이름 형식 변경 및 CORS CONTENT_DISPOSITION 노출로 설정 변경
    다큐먼트컨트롤러
    -파일이름 타입에 따라 구분하도록 변경
      -0이면 파일이름 사용
      -1이면 이름(학번)_과목명으로 설정

--- src/main/java/com/example/backend/auth/config/SecurityConfig.java
+++ src/main/java/com/example/backend/auth/config/SecurityConfig.java
@@ -81,6 +81,7 @@ public class SecurityConfig {
     config.setAllowedOrigins(client);
     config.setAllowedMethods(Arrays.asList("POST", "GET", "PATCH", "DELETE", "PUT"));
     config.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE));
+    config.setExposedHeaders(Arrays.asList(HttpHeaders.CONTENT_DISPOSITION));
     config.setAllowCredentials(true);
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
