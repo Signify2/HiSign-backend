3374f93a681f69b9fe6d212e9f0ac025cde0951d
kayoung-dev <kayoung150@gmail.com>
2026-06-13 00:30:36 +0900

    feat: remove 22200256 from AdminList

--- src/main/java/com/example/backend/auth/config/AdminList.java
+++ src/main/java/com/example/backend/auth/config/AdminList.java
@@ -1,15 +1,13 @@
 package com.example.backend.auth.config;
 
-import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 import java.util.List;
+
+import org.springframework.stereotype.Component;
 
 @Component
 public class AdminList {
     public static final List<String> ADMIN_UNIQUE_IDS = Arrays.asList(
             "50666",
-            "50654",
-            "22200256"
+            "50654"
     );
 }
